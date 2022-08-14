72847584b89bdf0f751101be075a3aaabd28b93b
MiloszStochaj <milosz.stochaj@gmail.com>
2022-08-14 19:35:59 +0200

    components addition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardFormComponent } from './card-form/card-form.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardFormComponent,
+    BusinessCardComponent
   ],
   imports: [
     BrowserModule,
