bbca413d320be35bef2dce824c0620f142ebf95d
MiloszStochaj <milosz.stochaj@gmail.com>
2022-08-14 21:37:42 +0200

    menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { BusinessCardComponent } from './business-card/business-card.component';
+import { CardFormComponent } from './card-form/card-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+  {
+    path: 'businnesCard',
+    component: BusinessCardComponent,
+  },
+  {
+    path: 'cardForm',
+    component: CardFormComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,23 @@ import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { BusinessCardComponent } from './business-card/business-card.component';
 
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+
 @NgModule({
-  declarations: [
-    AppComponent,
-    CardFormComponent,
-    BusinessCardComponent
-  ],
+  declarations: [AppComponent, CardFormComponent, BusinessCardComponent],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule, 
+    AppRoutingModule, 
+    BrowserAnimationsModule, 
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
