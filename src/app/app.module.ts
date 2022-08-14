fb0571bee8fa31c48162971962d0ccb63df822bb
MiloszStochaj <milosz.stochaj@gmail.com>
2022-08-14 21:51:32 +0200

    generate homeComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, CardFormComponent, BusinessCardComponent],
+  declarations: [AppComponent, CardFormComponent, BusinessCardComponent, HomeComponent],
   imports: [
     BrowserModule, 
     AppRoutingModule, 
