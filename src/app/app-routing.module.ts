5de04b98d136bdfa344c57caf64adbbe205c0481
MiloszStochaj <milosz.stochaj@gmail.com>
2022-08-15 19:50:15 +0200

    routing update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { BusinessCardComponent } from './business-card/business-card.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { HomeComponent } from './home/home.component';
@@ -17,6 +16,11 @@ const routes: Routes = [
   {
     path: 'cardForm',
     component: CardFormComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
   },
 ];
 
