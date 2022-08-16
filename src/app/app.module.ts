06951e839463a7b118d06d47d144e8d8b4309e57
MiloszStochaj <milosz.stochaj@gmail.com>
2022-08-16 09:53:23 +0200

    form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardFormComponent } from './card-form/card-form.component';
 import { BusinessCardComponent } from './business-card/business-card.component';
+import { HomeComponent } from './home/home.component';
 
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { HomeComponent } from './home/home.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+
 
 @NgModule({
   declarations: [AppComponent, CardFormComponent, BusinessCardComponent, HomeComponent],
@@ -22,6 +25,9 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     MatToolbarModule,
     MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/card-form/card-form.component.ts
+++ src/app/card-form/card-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 import { fade, slide } from '../animations';
 
 @Component({
@@ -11,6 +12,15 @@ import { fade, slide } from '../animations';
 ]
 })
 export class CardFormComponent implements OnInit {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
 
   constructor() { }
 
