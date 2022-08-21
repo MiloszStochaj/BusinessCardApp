1253d06dfd8cdb70dc5cd05bafe8cab2d3df24a7
MiloszStochaj <milosz.stochaj@gmail.com>
2022-08-21 22:21:27 +0200

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyCrR15hp9FRJ_2xJzvEvNwLRNSWXTdwqI4",
+    authDomain: "businesscardapp-6c121.firebaseapp.com",
+    projectId: "businesscardapp-6c121",
+    storageBucket: "businesscardapp-6c121.appspot.com",
+    messagingSenderId: "96763645793",
+    appId: "1:96763645793:web:f89245176c98d0ea393f20"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyCrR15hp9FRJ_2xJzvEvNwLRNSWXTdwqI4",
+    authDomain: "businesscardapp-6c121.firebaseapp.com",
+    projectId: "businesscardapp-6c121",
+    storageBucket: "businesscardapp-6c121.appspot.com",
+    messagingSenderId: "96763645793",
+    appId: "1:96763645793:web:f89245176c98d0ea393f20"
+  }
 };
 
 /*
